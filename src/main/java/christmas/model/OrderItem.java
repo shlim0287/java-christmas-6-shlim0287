6dab81577076aa06499af22745399882d1d30181
sunghyun <shlim0287@naver.com>
2023-11-11 21:57:55 +0900

    feat : 고객이 주문한 메뉴들 중 메뉴에 관한 클래스 생성, 주문한 메뉴의 총 금액 계산 메서드 추가.

---
+++ src/main/java/christmas/model/OrderItem.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+import christmas.model.menu.MenuItem;
+
+public class OrderItem {
+    private final MenuItem menuItem;
+    private final int quantity;
+
+    public OrderItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice(){
+        return menuItem.getPrice()*quantity;
+    }
+}
