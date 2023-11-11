5add7e7c8db326f31749606cc9f21dde2c2df529
sunghyun <shlim0287@naver.com>
2023-11-11 23:01:54 +0900

    feat : 쉼표(,) 구분자로 구분한 메뉴별 주문음식과 개수 형식 검증하는 메서드 추가.

--- src/main/java/christmas/validator/ValidateMenuAndQuantity.java
+++ src/main/java/christmas/validator/ValidateMenuAndQuantity.java
@@ -1,10 +1,12 @@
 package christmas.validator;
 
+import java.util.StringTokenizer;
+
 public class ValidateMenuAndQuantity {
     public static void validate(String input){
         validateEmpty(input);
         validateOrderFormat(input);
-
+        validateMenuOrders(input);
     }
 
     private static void validateEmpty(String input){
@@ -21,6 +23,24 @@ public class ValidateMenuAndQuantity {
 
     private static boolean isMatchesOrderFormat(String input) {
         return input.matches("^[^,]+-\\d+(,[^,]+-\\d+)*$");
+    }
+
+    private static void validateMenuOrders(String input){
+        StringTokenizer tokenizer=new StringTokenizer(input,",");
+        while(tokenizer.hasMoreTokens()){
+            validateMenuOrder(tokenizer);
+        }
+
+    }
+
+    private static void validateMenuOrder(StringTokenizer tokenizer) {
+        if(!isMatchesMenuOrder(tokenizer.nextToken())){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isMatchesMenuOrder(String input) {
+        return input.matches("^[^,]+-\\d+$");
     }
 
 }
