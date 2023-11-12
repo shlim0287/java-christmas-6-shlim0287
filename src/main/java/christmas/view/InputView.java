233f413849d3311bff0a5800cf72be9ad68a0806
sunghyun <shlim0287@naver.com>
2023-11-13 01:07:59 +0900

    feat : 예상방문날짜 입력메서드 추가.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,13 +2,19 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.model.OrderItem;
+import christmas.validator.ValidateMenuAndQuantity;
+import christmas.validator.ValidateVisitDate;
 import java.util.List;
+import org.mockito.internal.matchers.Or;
 
 public class InputView {
 
     public int requestVisitDate(){
-        return Integer.parseInt(console());
+        String input = console();
+        ValidateVisitDate.validate(input);
+        return Integer.parseInt(input);
     }
+
 
 
     private String console(){
