708f6fa11da8e1f1642ffe1a282d07992cde2f93
sunghyun <shlim0287@naver.com>
2023-11-13 14:27:12 +0900

    feat : 음료만 주문했을 때 예외처리 하는 메서드 추가.

--- src/main/java/christmas/validator/ValidateOrderInput.java
+++ src/main/java/christmas/validator/ValidateOrderInput.java
@@ -1,13 +1,13 @@
 package christmas.validator;
 
 import christmas.model.OrderItem;
-import christmas.model.OrderItems;
+import christmas.model.menu.MenuType;
 import java.util.List;
 
 public class ValidateOrderInput {
     public static void validate(List<OrderItem> orderItems){
         validateTotalMenuCountExceedsLimit(orderItems);
-        validateOnlyBeverages();
+        validateOnlyBeverages(orderItems);
     }
 
     private static void validateTotalMenuCountExceedsLimit(List<OrderItem> orderItems) {
@@ -24,6 +24,25 @@ public class ValidateOrderInput {
         return totalQuantity;
     }
 
-    private static void validateOnlyBeverages() {
+    private static void validateOnlyBeverages(List<OrderItem> orderItems) {
+        int count=0;
+        for(OrderItem orderItem:orderItems){
+            MenuType menuType = orderItem.getMenuItem().getMenuType();
+            count = countNonBeverage(count, menuType);
+        }
+        validateNonBeverage(count);
+    }
+
+    private static int countNonBeverage(int count, MenuType menuType) {
+        if(menuType.toString()!="BEVERAGE"){
+            count++;
+        }
+        return count;
+    }
+
+    private static void validateNonBeverage(int count) {
+        if(count ==0){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
