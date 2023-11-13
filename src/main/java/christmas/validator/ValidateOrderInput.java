694306e0a7f271beaf7ccebe476a7eb7158361c1
sunghyun <shlim0287@naver.com>
2023-11-13 13:35:27 +0900

    feat : 메뉴와 개수 입력에 대한 추가 검증 클래스 뼈대 추가.

---
+++ src/main/java/christmas/validator/ValidateOrderInput.java
@@ -0,0 +1,18 @@
+package christmas.validator;
+
+public class ValidateOrderInput {
+    public static void validate(){
+        validateMenuCountZero();
+        validateTotalMenuCountExceedsLimit();
+        validateOnlyBeverages();
+    }
+
+    private static void validateMenuCountZero() {
+    }
+
+    private static void validateTotalMenuCountExceedsLimit() {
+    }
+
+    private static void validateOnlyBeverages() {
+    }
+}
