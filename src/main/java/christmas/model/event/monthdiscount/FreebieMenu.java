faccede57ca2fc4f425d7ffd9466a3fc43c1ff78
sunghyun <shlim0287@naver.com>
2023-11-13 23:06:35 +0900

    refactor : Event 인터페이스 적용 및 메서드 오버라이딩 기능 구현.

--- src/main/java/christmas/model/event/monthdiscount/FreebieMenu.java
+++ src/main/java/christmas/model/event/monthdiscount/FreebieMenu.java
@@ -2,10 +2,21 @@ package christmas.model.event.monthdiscount;
 
 import christmas.model.OrderItem;
 import christmas.model.OrderItems;
+import christmas.model.event.Event;
 import christmas.model.menu.MenuItem;
 
-public class FreebieMenu {
+public class FreebieMenu implements Event {
     static final int APPLICABLE_FREEBIE_AMOUNT=120000;
+
+    @Override
+    public int calculateDiscount(OrderItems orderItems, int day) {
+        OrderItem orderItem = applicableOrNot(orderItems);
+        if(orderItem!=null){
+            return orderItem.getTotalPrice();
+        }
+        return 0;
+    }
+
     public static OrderItem applicableOrNot (OrderItems orderItems) {
         int totalOrderPrice= orderItems.getTotalPriceOfOrder(orderItems);
         int freebieCount=totalOrderPrice/APPLICABLE_FREEBIE_AMOUNT;
@@ -23,4 +34,5 @@ public class FreebieMenu {
     private static boolean hasFreebie(int freebieCount) {
         return freebieCount > 0;
     }
+
 }
