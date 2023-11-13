3831f1f187a23aaa0e00d03e4b6f0662b32cb05c
sunghyun <shlim0287@naver.com>
2023-11-13 14:12:51 +0900

    feat : 메뉴의 총 주문 개수가 20이 초과 될 때 예외처리 메서드 추가.

--- src/main/java/christmas/validator/ValidateOrderInput.java
+++ src/main/java/christmas/validator/ValidateOrderInput.java
@@ -1,16 +1,27 @@
 package christmas.validator;
 
+import christmas.model.OrderItem;
+import christmas.model.OrderItems;
+import java.util.List;
+
 public class ValidateOrderInput {
-    public static void validate(){
-        validateMenuCountZero();
-        validateTotalMenuCountExceedsLimit();
+    public static void validate(List<OrderItem> orderItems){
+        validateTotalMenuCountExceedsLimit(orderItems);
         validateOnlyBeverages();
     }
 
-    private static void validateMenuCountZero() {
+    private static void validateTotalMenuCountExceedsLimit(List<OrderItem> orderItems) {
+        if(getTotalQuantityOfMenuItems(orderItems)>20){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 
-    private static void validateTotalMenuCountExceedsLimit() {
+    private static int getTotalQuantityOfMenuItems(List<OrderItem> orderItems) {
+        int totalQuantity = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalQuantity += orderItem.getQuantity();
+        }
+        return totalQuantity;
     }
 
     private static void validateOnlyBeverages() {
