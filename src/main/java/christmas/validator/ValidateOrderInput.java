97e1282162660aa78c6aae96578a56a4f2fa277f
sunghyun <shlim0287@naver.com>
2023-11-13 14:50:57 +0900

    refactor : 검증하는 입력값의 타입을 OrderItems로 수정.

--- src/main/java/christmas/validator/ValidateOrderInput.java
+++ src/main/java/christmas/validator/ValidateOrderInput.java
@@ -1,32 +1,27 @@
 package christmas.validator;
 
 import christmas.model.OrderItem;
+import christmas.model.OrderItems;
 import christmas.model.menu.MenuType;
 import java.util.List;
 
 public class ValidateOrderInput {
-    public static void validate(List<OrderItem> orderItems){
+    public static void validate(OrderItems orderItems){
         validateTotalMenuCountExceedsLimit(orderItems);
         validateOnlyBeverages(orderItems);
     }
 
-    private static void validateTotalMenuCountExceedsLimit(List<OrderItem> orderItems) {
-        if(getTotalQuantityOfMenuItems(orderItems)>20){
+    private static void validateTotalMenuCountExceedsLimit(OrderItems orderItems) {
+        if(orderItems.getTotalQuantityOfMenuItems(orderItems.getOrderItems())>20){
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    private static int getTotalQuantityOfMenuItems(List<OrderItem> orderItems) {
-        int totalQuantity = 0;
-        for (OrderItem orderItem : orderItems) {
-            totalQuantity += orderItem.getQuantity();
-        }
-        return totalQuantity;
-    }
 
-    private static void validateOnlyBeverages(List<OrderItem> orderItems) {
+
+    private static void validateOnlyBeverages(OrderItems orderItems) {
         int count=0;
-        for(OrderItem orderItem:orderItems){
+        for(OrderItem orderItem:orderItems.getOrderItems()){
             MenuType menuType = orderItem.getMenuItem().getMenuType();
             count = countNonBeverage(count, menuType);
         }
