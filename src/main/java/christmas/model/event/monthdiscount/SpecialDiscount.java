c2828b2a284b734907291871efd8e288c66af51b
sunghyun <shlim0287@naver.com>
2023-11-13 22:53:02 +0900

    refactor : Event인터페이스 적용 후 non-static으로 수정.

--- src/main/java/christmas/model/event/monthdiscount/SpecialDiscount.java
+++ src/main/java/christmas/model/event/monthdiscount/SpecialDiscount.java
@@ -1,4 +1,23 @@
 package christmas.model.event.monthdiscount;
 
-public class SpecialDiscount {
+import christmas.model.OrderItem;
+import christmas.model.event.Event;
+import java.time.LocalDate;
+import java.util.Set;
+
+public class SpecialDiscount implements Event {
+    private static final Set<Integer> SPECIAL_DISCOUNT_DAYS=Set.of(3,10,17,24,25,31);
+    private static final int SPECIAL_DISCOUNT_AMOUNT=1000;
+
+    @Override
+    public int calculateDiscount(OrderItem orderItem,int day){
+        if(isSpecialDiscountDay(day)){
+            return SPECIAL_DISCOUNT_AMOUNT;
+        }
+        return 0;
+    }
+
+    private boolean isSpecialDiscountDay(int day){
+        return SPECIAL_DISCOUNT_DAYS.contains(day);
+    }
 }
