c173772cd703ab80b431bf04be834ce49ac0f5f8
sunghyun <shlim0287@naver.com>
2023-11-13 23:02:53 +0900

    refactor : Event 인터페이스 적용 및 파라미터 수정.

--- src/main/java/christmas/model/event/ChristmasDiscount.java
+++ src/main/java/christmas/model/event/ChristmasDiscount.java
@@ -1,12 +1,14 @@
 package christmas.model.event;
 
-public class ChristmasDiscount {
+import christmas.model.OrderItems;
+
+public class ChristmasDiscount implements Event{
     private static final int INITIAL_DISCOUNT_AMOUNT=1000;
     private static final int ADDITIONAL_DISCOUNT_PER_DAT=100;
     private static final int START_DATE=1;
     private static final int END_DATE=25;
-
-    public static int calculateDiscount(int day){
+    @Override
+    public int calculateDiscount(OrderItems orderItems, int day){
         if(day>=START_DATE && day<=END_DATE){
             int dayBetween=day-1;
             return INITIAL_DISCOUNT_AMOUNT+(dayBetween*ADDITIONAL_DISCOUNT_PER_DAT);
