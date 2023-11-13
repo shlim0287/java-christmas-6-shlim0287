2463ae552dfef1cf29f71ef54a98492b896257ce
sunghyun <shlim0287@naver.com>
2023-11-13 15:57:17 +0900

    refactor : 입력받는 파라미터 수정.

--- src/main/java/christmas/model/event/monthdiscount/WeeklyDiscount.java
+++ src/main/java/christmas/model/event/monthdiscount/WeeklyDiscount.java
@@ -1,5 +1,6 @@
 package christmas.model.event.monthdiscount;
 
+import christmas.model.OrderItems;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
@@ -7,24 +8,24 @@ public class WeeklyDiscount {
     private static final int WEEKDAY_DISCOUNT_AMOUNT=2023;
     private static final int WEEKEND_DISCOUNT_AMOUNT=2023;
 
-    public static int calculateDiscount(int day){
+    public static int calculateDiscount(int day, OrderItems orderItems){
         LocalDate date=LocalDate.of(2023,12,day);
         DayOfWeek dayOfWeek=date.getDayOfWeek();
 
         if(isWeekend(dayOfWeek)){
-            return applyWeekendDiscount(day);
+            return applyWeekendDiscount(orderItems);
         }
         if(!isWeekend(dayOfWeek)){
-            return applyWeekdayDiscount(day);
+            return applyWeekdayDiscount(orderItems);
         }
         return 0;
     }
 
-    private static int applyWeekendDiscount(int day) {
+    private static int applyWeekendDiscount(OrderItems orderItems) {
         return 0;
     }
 
-    private static int applyWeekdayDiscount(int day) {
+    private static int applyWeekdayDiscount(OrderItems orderItems) {
         return 0;
     }
 
