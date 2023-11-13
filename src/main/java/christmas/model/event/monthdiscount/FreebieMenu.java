da27d00de6ed4d2e6490fc4d403b0bf9e2e90974
sunghyun <shlim0287@naver.com>
2023-11-13 17:53:26 +0900

    feat : 증정 이벤트 기능구현.

--- src/main/java/christmas/model/event/monthdiscount/FreebieMenu.java
+++ src/main/java/christmas/model/event/monthdiscount/FreebieMenu.java
@@ -1,4 +1,26 @@
 package christmas.model.event.monthdiscount;
 
+import christmas.model.OrderItem;
+import christmas.model.OrderItems;
+import christmas.model.menu.MenuItem;
+
 public class FreebieMenu {
+    static final int APPLICABLE_FREEBIE_AMOUNT=120000;
+    public static OrderItem applicableOrNot (OrderItems orderItems) {
+        int totalOrderPrice= orderItems.getTotalPriceOfOrder(orderItems);
+        int freebieCount=totalOrderPrice/APPLICABLE_FREEBIE_AMOUNT;
+        return checkApplicable(freebieCount);
+    }
+
+    private static OrderItem checkApplicable(int freebieCount) {
+        if(hasFreebie(freebieCount)){
+            OrderItem orderItem = new OrderItem(MenuItem.CHAMPAGNE, freebieCount);
+            return orderItem;
+        }
+        return null;
+    }
+
+    private static boolean hasFreebie(int freebieCount) {
+        return freebieCount > 0;
+    }
 }
