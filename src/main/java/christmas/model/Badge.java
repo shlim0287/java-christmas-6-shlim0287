88680346db99a43c1ca4863326ecbf208e0af596
sunghyun <shlim0287@naver.com>
2023-11-14 16:16:28 +0900

    feat : 12월 이벤트 배지 부여 클래스 생성 및 기능 구현.

---
+++ src/main/java/christmas/model/Badge.java
@@ -0,0 +1,32 @@
+package christmas.model;
+
+public class Badge {
+    private static final int STAR_THRESHOLD = 5000;
+    public static final int TREE_THRESHOLD = 10000;
+    public static final int SANTA_THRESHOLD = 20000;
+
+    private static String getCategory(int totalDiscount) {
+        if (isStar(totalDiscount)) {
+            return "별";
+        }
+        if (isTree(totalDiscount)) {
+            return "트리";
+        }
+        if (isSanta(totalDiscount)) {
+            return "산타";
+        }
+        return "없음";
+    }
+
+    private static boolean isStar(int totalDiscount) {
+        return totalDiscount >= STAR_THRESHOLD && totalDiscount < TREE_THRESHOLD;
+    }
+
+    private static boolean isTree(int totalDiscount) {
+        return totalDiscount >= TREE_THRESHOLD && totalDiscount < SANTA_THRESHOLD;
+    }
+
+    private static boolean isSanta(int totalDiscount) {
+        return totalDiscount >= SANTA_THRESHOLD;
+    }
+}
