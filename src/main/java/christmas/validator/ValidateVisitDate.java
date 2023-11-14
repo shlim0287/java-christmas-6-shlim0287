77e09527662313e8d2f923647e0a5c1fc6e5f97a
sunghyun <shlim0287@naver.com>
2023-11-14 16:35:10 +0900

    refactor : 예외처리 출력문구 수정.

--- src/main/java/christmas/validator/ValidateVisitDate.java
+++ src/main/java/christmas/validator/ValidateVisitDate.java
@@ -12,7 +12,7 @@ public class ValidateVisitDate {
 
     private static void validateEmpty(String input) {
         if(input.isBlank()){
-            throw new IllegalArgumentException("[ERROR] 입력된 값이 없습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
@@ -35,7 +35,7 @@ public class ValidateVisitDate {
 
     private static void validateNumerical(String input) {
         if(isNotNumerical(input)){
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
