9c6a5189b592f4e9418ac6a8f10a9410282504ea
sunghyun <shlim0287@naver.com>
2023-11-14 23:33:30 +0900

    feat : 컨트롤러를 통해 메인 메서드에서 동작 구현.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.model.OrderItem;
+import christmas.model.OrderItems;
+import christmas.model.event.Event;
+import christmas.model.event.EventManager;
+import christmas.model.event.monthdiscount.FreebieMenu;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ChristmasController controller=new ChristmasController();
+        controller.run();
     }
 }
